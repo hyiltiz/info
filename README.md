f90ea19a4f7a4c5ce1f5e1a44fb1c57b5edaa448
Hörmetjan Yiltiz <hyiltiz@gmail.com>
2021-02-27 05:49:14 -0500

    initial commit
    Signed-off-by: Hörmetjan Yiltiz <hyiltiz@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
-# info
-Correctly computed entropy, mutual information, and surprisal, all-in-one module.
+# Quick start
+Download the module `info.py` into your project directory, and do:
+```
+from info import compute as compute_mutual_information
+
+X = [1, 2, 3, 4, 5]
+Y = [4, 1, 3, 4, 2]
+X_range = (1, 5)
+Y_range = (1, 5)  # 5 did not occur in the sample Y yet
+
+[mi_XY, mi_XY_normalized, everything] = compute_mutual_information(X, Y, [X_range, Y_range])
+
+# `everything` is a nested dictionary that contains other normalized
+# variants of mutual information, as well as some commonly used information
+# theoretic metrics such as entropy, conditional entropy, self-information
+# (also known as information content or surprisal). Joint and marginal
+# probability mass functions and contingency tables are also included for
+# convenience.
+
+everything = {
+ 'N': 1000,
+ 'entropy': {'X':         2.3168095633619847,
+             'Y':         2.3199539124456714,
+             'XY':        4.625700518983682,
+             'X-given-Y': 2.3088909556216985,
+             'Y-given-X': 2.305746606538012},
+ 'mutual-information': {
+             'standard':                        0.01106295682397285,
+              # normalize by entropy (asymmetric), ranges from [0, 1]
+             'normalized-by-HX':                0.0047750825095521,
+             'normalized-by-HY':                0.0047750825095521,
+              # other normalization variants (symmetric)
+             'symmetric':                       0.004771844361565519,
+             'redundancy':                      0.0023859221807827594,
+             'generalized-pearson-correlation': 0.004771845458771204,
+             'generalized-total-correlation':   0.0047750825095521,
+             'information-quality-ratio':       0.0023916284200784154,
+             'adjusted-mutual-information':     'TODO'},
+ 'self-information': {'X': array([2.35845397, 2.41119543, 2.23786383, 2.48196851, 2.14560532]),
+                      'Y': array([2.29335894, 2.42662547, 2.32192809, 2.20423305, 2.37332725]),
+                      'XY': array([[4.83650127, 4.50635267, 4.57346686, 4.68038207, 4.50635267],
+                                   [4.53951953, 5.01158797, 4.83650127, 4.87832144, 4.53951953],
+                                   [4.57346686, 4.96578428, 4.60823228, 5.15842936, 4.13289427],
+                                   [4.64385619, 4.44222233, 4.21089678, 4.79585928, 4.60823228],
+                                   [4.83650127, 4.83650127, 4.64385619, 4.57346686, 4.60823228]])}
+ 'count': {'X': array([[195, 188, 212, 179, 226],
+                       [195, 188, 212, 179, 226],
+                       [195, 188, 212, 179, 226],
+                       [195, 188, 212, 179, 226],
+                       [195, 188, 212, 179, 226]]),
+           'Y': array([[204, 204, 204, 204, 204],
+                       [186, 186, 186, 186, 186],
+                       [200, 200, 200, 200, 200],
+                       [217, 217, 217, 217, 217],
+                       [193, 193, 193, 193, 193]])},
+           'XY': array([[35, 44, 42, 39, 44],
+                        [43, 31, 35, 34, 43],
+                        [42, 32, 41, 28, 57],
+                        [40, 46, 54, 36, 41],
+                        [35, 35, 40, 42, 41]]),
+ 'p': {'X': array([[0.195, 0.188, 0.212, 0.179, 0.226],
+                   [0.195, 0.188, 0.212, 0.179, 0.226],
+                   [0.195, 0.188, 0.212, 0.179, 0.226],
+                   [0.195, 0.188, 0.212, 0.179, 0.226],
+                   [0.195, 0.188, 0.212, 0.179, 0.226]]),
+       'Y': array([[0.204, 0.204, 0.204, 0.204, 0.204],
+                   [0.186, 0.186, 0.186, 0.186, 0.186],
+                   [0.2  , 0.2  , 0.2  , 0.2  , 0.2  ],
+                   [0.217, 0.217, 0.217, 0.217, 0.217],
+                   [0.193, 0.193, 0.193, 0.193, 0.193]])}
+       'XY': array([[0.035, 0.044, 0.042, 0.039, 0.044],
+                    [0.043, 0.031, 0.035, 0.034, 0.043],
+                    [0.042, 0.032, 0.041, 0.028, 0.057],
+                    [0.04 , 0.046, 0.054, 0.036, 0.041],
+                    [0.035, 0.035, 0.04 , 0.042, 0.041]]),
+}
+```
---
+++ info.py
@@ -0,0 +1,153 @@
+"""
+Compute mutual information and other relevant information metrics for two random variables.
+"""
+
+__author__ = "Augustin Burchell, Hörmet Yiltiz"
+__copyright__ = "Copyright (C) 2021 Hörmet Yiltiz"
+__license__ = "GNU GPL version 3 or later"
+__version__ = "0.9"
+
+from itertools import product
+import numpy as np
+
+# TODO: add `hypothesis` property tests for mutual information and entropy
+
+
+def compute(X: list, Y: list, domain: list):
+    """Computes the mutual information between two random variables, defined as:
+    sum pXY log(pXY/(pX pY)) in bits. Returns a tuple that contains:
+      - mutual information
+      - mutual information normalized by the entropy of X (useful if X is
+        signal)
+      - a nested dictionary that contains other normalized variants of mutual
+        information, as well as some commonly used information theoretic
+        metrics such as entropy, conditional entropy, self-information (also
+        known as information content or surprisal). Joint and marginal
+        probability mass functions and contingency tables are also included for
+        convenience.
+
+    Copyright (C) Hörmet Yiltiz <hyiltiz@gmail.com>, 2021.
+    """
+    # TODO generalize for N variables; currently it assumes the X and Y are in
+    # the same encoding/dictionary/domain generalize the input arguments s.t.
+    # it works for ND vectors, like:
+    # compute_mutual_information([X, Y, ...],
+    #                            [(range(of X)),(domain of Y),...])
+
+    observed = list(zip(X, Y))
+    N = len(observed)
+
+    # NOTE: range() upper range is larger by 1
+    grid = product(range(domain[0][0], domain[0][1]+1),
+                   range(domain[0][0], domain[0][1]+1))
+
+    # compute the joint frequency table
+    observed_counts = [observed.count(coord) for coord in grid]
+    cXY = np.array(observed_counts).reshape(domain[0][1], domain[1][1])
+
+    # compute the marginal counts
+    [cX, cY] = np.meshgrid(cXY.sum(axis=0), cXY.sum(axis=1))
+
+    # now normalize into probability mass functions
+    [pXY, pX, pY] = [cXY/N, cX/N, cY/N]
+    pXpY = pX * pY
+
+    # get rid of the zero events (only in the joint is sufficient)
+    # 0 * log(0/[possibly zero]) := 0 for information
+    # this limit actually comes from the expectation definition of information
+    nonzeros = pXY != 0
+    nonzeros_X = pX[0, :] != 0
+    nonzeros_Y = pY[:, 0] != 0
+
+    # -inf < pmi < min(-log(pX), -log(pY))
+    pointwise_mutual_information = np.log2(pXY[nonzeros]/pXpY[nonzeros])
+
+    # the expected value of the pointwise MI
+    mutual_XY = sum(pXY[nonzeros] * pointwise_mutual_information)
+
+    # now compute other relevant information metrics
+    # the self-information
+    self_information_XY = -np.log2(pXY)
+    self_information_X = -np.log2(pX[0, :])
+    self_information_Y = -np.log2(pY[:, 0])
+
+    # entropy := expected self information
+    HX = sum(pX[0, :][nonzeros_X] * self_information_X[nonzeros_X])
+    HY = sum(pY[:, 0][nonzeros_Y] * self_information_Y[nonzeros_Y])
+    HXY = sum(pXY[nonzeros] * self_information_XY[nonzeros])
+
+    # as a reference and to prevent confusion, we provide most of the relevant
+    # values in probability and information theory here.
+    # See for more information:
+    # https://en.wikipedia.org/wiki/Mutual_information
+    # https://en.wikipedia.org/wiki/Entropy_(information_theory)
+    # https://en.wikipedia.org/wiki/Information_content for self-information
+    out = {'N': N,
+           'entropy': {  # expected self-information; non-negative
+               'XY': HXY,
+               'X': HX,
+               'Y': HY,
+               'X-given-Y': -sum(pXY[nonzeros] * np.log2(
+                   pXY[nonzeros]/pX[nonzeros])),
+               'Y-given-X': -sum(pXY[nonzeros] * np.log2(
+                   pXY[nonzeros]/pY[nonzeros]))},
+           'mutual-information': {
+               'standard': mutual_XY,  # non-negative
+               # normalize by entropy (asymmetric), ranges from [0, 1]
+               'normalized-by-HX': mutual_XY/HX,
+               'normalized-by-HY': mutual_XY/HX,
+               # other normalization variants (symmetric)
+               'redundancy': mutual_XY/(HX+HY),
+               'symmetric': 2*mutual_XY/(HX+HY),
+               'generalized-total-correlation': mutual_XY/min(HX, HY),
+               'generalized-pearson-correlation': mutual_XY/np.sqrt(HX*HY),
+               'information-quality-ratio': mutual_XY/HXY,
+               # AMI(X,Y) = [MI(X,Y)-E(MI(X,Y))]/[avg(H(X),H(Y))-E(MI(X,Y))]'
+               'adjusted-mutual-information': 'TODO'},
+           'self-information': {  # could be inf if p=0
+               'X': self_information_X,
+               'Y': self_information_Y},
+               'XY': self_information_XY,
+           'count': {
+               'X': cX,
+               'Y': cY},
+               'XY': cXY,
+           'p': {
+               'X': pX,
+               'Y': pY}
+               'XY': pXY,
+           }
+
+    # print(f"====\nmutual_XY:{mutual_XY}, "
+    #       f"normalied by entropy(X): {mutual_XY/HX}\n, "
+    #       f"{out['mutual-information']}")
+    # import ipdb; ipdb.set_trace() # BREAKPOIN for debugging
+
+    return (mutual_XY, mutual_XY/HX, out)
+
+def main():
+    # example function call
+    domain = [(1, 5), (1, 5)]
+    samples = 1000
+    X = np.random.choice(range(domain[0][0], domain[0][1]+1),
+                         samples, replace=True)
+    Y = np.random.choice(range(domain[1][0], domain[1][1]+1),
+                         samples, replace=True)
+    identical = X
+    shuffled = np.random.choice(X, size=len(X), replace=False)
+
+    # test for properties: symmetry, non-negativity
+    mi_X_self = compute(X, identical, domain)
+    mi_self_X = compute(identical, X, domain)
+
+    mi_X_R = compute(X, shuffled, domain)
+    mi_R_X = compute(shuffled, X, domain)
+
+    mi_X_Y = compute(X, Y, domain)
+    mi_Y_X = compute(Y, X, domain)
+
+    [mi_X_Y, normalized, info] = compute(X, Y, domain)
+    print(mi_X_Y)
+
+if __name__ == '__main__':
+    main()
